075f54568658274db46288a2ee9e74a330e89b2d
BeforeSunrise710 <zhangyijie710@outlook.com>
2023-08-13 21:03:24 -0400

    new branch

--- neurips2020/models/depth/deterministic.py
+++ neurips2020/models/depth/deterministic.py
@@ -5,7 +5,7 @@ from tensorflow.keras.layers import Conv2D, MaxPooling2D, \
 import functools
 from evidential_deep_learning.layers import Conv2DNormal
 
-def create(input_shape, activation=tf.nn.relu, num_class=1):
+def create(input_shape, activation=tf.nn.relu, num_class=1):    
     opts = locals().copy()
     model, opts = dropout.create(input_shape, drop_prob=0.0, sigma=False, activation=activation, num_class=num_class)
     return model, opts
